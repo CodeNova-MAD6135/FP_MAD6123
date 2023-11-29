680bb5ced54287e2e99b9077d71299157082187f
Sonia364 <c0872364@mylambton.ca>
2023-11-28 23:36:32 -0500

    Added project's cost as line graph

--- app/components/Admin/ProjectOverview.js
+++ app/components/Admin/ProjectOverview.js
@@ -1,59 +1,147 @@
-import React, { useState ,useEffect} from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import ProjectCard from './ProjectCard';
-import { getCurrentUser, getMyProjectList } from '../../data/Storage';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Dimensions, ScrollView } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+} from 'react-native-chart-kit';
+import { getCurrentUser, getMyProjectList, getUserByID } from '../../data/Storage';
+import { useFocusEffect } from '@react-navigation/native';
 
+const ProjectOverview = ({ route }) => {
+  const [userId, setUserId] = useState('');
+  const [projects, setProjects] = useState([]);
+  const [linedata, setLineChartData] = useState();
 
-const ProjectOverview = ({route}) => {
+  // const linedata = {
+  //   labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  //   datasets: [
+  //     {
+  //       data: [20, 45, 28, 80, 99, 43],
+  //       strokeWidth: 2, // optional
+  //     },
+  //   ],
+  // };
 
-  const [userId,setUserId] = useState('');
+  useEffect(() => {
 
-  // Dummy project data for demonstration
-  const [projects, setProjects] = useState([
-    { id: 1, name: 'Project A', status: 'In Progress', progress: 70 },
-    { id: 2, name: 'Project B', status: 'Completed', progress: 100 },
-    // Add more project objects as needed
-  ])
+    const getUser = async () => {
+      const user = await getCurrentUser();
+      setUserId(user.id);
+    };
 
-  const getUser = async() => {
-    const user = await getCurrentUser()
-    setUserId(user.id)
-  }
-  useEffect( () => {
+    getUser();
 
-    getUser()
-
-    const loadProjects = async() => {
-      const projects = await getMyProjectList(userId)
-      console.log(projects)
-      setProjects(projects)
+    const loadProjects = async () => {
+      const allProjects = await getMyProjectList(userId);
+      setProjects(allProjects);
     };
 
     loadProjects();
-  },[userId])
+
+  }, [userId]);
+
+  useEffect(() => {
+    const calculateProjectCostsForLineChart = async () => {
+      const newLineChartData = {
+        labels: [],  // Project names as labels
+        datasets: [{ data: [], strokeWidth: 2 }],
+      };
+
+      // Iterate over projects
+      for (const project of projects) {
+        const completedTasks = project.tasks.filter((task) => task.status === 'Completed');
+        if (completedTasks && completedTasks.length > 0) {
+          const totalCost = await calculateTotalCost(completedTasks);
+
+          if (totalCost !== undefined) {
+
+            newLineChartData.labels.push(project.projectName);
+            newLineChartData.datasets[0].data.push(totalCost);
+
+          }
+        }
+      }
+
+      setLineChartData(newLineChartData);
+      console.log(linedata);
+
+    };
+
+    const calculateTotalCost = async (completedTasks) => {
+
+      const totalCost = await completedTasks.reduce(async (acc, task) => {
+        const member = await getUserByID(task.assignedMember);
+        const hourlyRate = member.rate;
+        const hoursWorked = task.completionHours;
+        return acc + hourlyRate * hoursWorked;
+      }, 0);
+
+      return totalCost;
+    };
+
+    calculateProjectCostsForLineChart();
+
+
+
+  }, [projects]);
+
+  useEffect(() => {
+    // Log the updated linedata when it changes
+    console.log(linedata);
+  }, [linedata]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+    }, [linedata])
+  );
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={projects}
-        keyExtractor={(item,index) => index.toString()}
-        renderItem={({ item }) => (
-          <ProjectCard
-            name={item.projectName}
-            status={item.status}
-            progress={item.progress}
+    <ScrollView>
+      {linedata && linedata.datasets && linedata.datasets[0].data.length > 0 && (
+        <View style={styles.lineChart}>
+          <LineChart
+            data={linedata}
+            width={420} // from react-native
+            height={220}
+            yAxisLabel={'$'}
+            chartConfig={{
+              backgroundColor: '#5D5FDE',
+              backgroundGradientFrom: '#5D5FDE',
+              backgroundGradientTo: '#5D5FDE',
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                marginVertical: 40,
+              }
+            }}
+            bezier
+            style={{
+              borderRadius: 5
+            }}
           />
-        )}
-      />
-    </View>
-  )};
+        </View>
+      )}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      padding: 20
-    }
-  });
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 8,
+    paddingTop: 30,
+    backgroundColor: '#ecf0f1',
+  },
+  lineChart: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  }
+});
 
 export default ProjectOverview;
+
